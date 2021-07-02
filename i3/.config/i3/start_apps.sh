74a21d15e8f6c6d23d862f332265eba7492b93e9
Hexalyse <reversebob@gmail.com>
2021-07-02 13:26:42 +0200

    Polybar now uses the correct temperature sensor every time

--- i3/.config/i3/start_apps.sh
+++ i3/.config/i3/start_apps.sh
@@ -1,9 +1,3 @@
 #!/bin/env bash
-for i in /sys/class/hwmon/hwmon*/temp*_input; do 
-    if [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "coretemp: Package id 0" ]; then
-        export HWMON_PATH="$i"
-    fi
-done
 pkill redshift-gtk; pkill redshift; redshift-gtk &
-pkill polybar; polybar -r i3-nord &
 spotify &
--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
-
-# Terminate already running bar instances
-killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
-
-# Launch bar1 and bar2
-echo "---" | tee -a /dev/shm/polybar1.log /dev/shm/polybar2.log
-polybar example -r >>/tmp/polybar1.log 2>&1 & disown
-
-echo "Bars launched..."
+for i in /sys/class/hwmon/hwmon*/temp*_input; do 
+    if [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "coretemp: Package id 0" ]; then
+        export HWMON_PATH="$i"
+    fi
+done
+echo $HWMON_PATH
+pkill polybar; polybar -r i3-nord &
