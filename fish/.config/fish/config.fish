5b6955e40f12c463b5b756e6a867c853b8a529ad
Hexalyse <reversebob@gmail.com>
2022-04-14 19:45:29 +0200

    fix a few things

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,5 @@
 starship init fish | source
 cat ~/.cache/wal/sequences &
-neofetch --disable gpu term_font cpu memory --ascii_distro manjaro_small
+neofetch --disable gpu term_font cpu memory
 set fish_greeting
-
 set -x SSH_AUTH_SOCK '/run/user/1000/gnupg/S.gpg-agent.ssh'
