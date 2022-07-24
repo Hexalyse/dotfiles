8874965facd768b04d100514df4fc86c9c141883
Hexalyse <reversebob@gmail.com>
2022-07-24 18:53:07 +0200

    fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,1 +1,6 @@
-../../dotfiles/fish/.config/fish/config.fish
+starship init fish | source
+cat ~/.cache/wal/sequences &
+neofetch --disable gpu term_font cpu memory
+set fish_greeting
+set -x SSH_AUTH_SOCK '/run/user/1000/gnupg/S.gpg-agent.ssh'
+set PATH $HOME/.cargo/bin $PATH
