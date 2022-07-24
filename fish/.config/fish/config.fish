321cc5f0a678595da54ec3a79589299a6420864b
Hexalyse <reversebob@gmail.com>
2022-07-24 15:22:57 +0200

    Update i3-wal config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,3 +3,4 @@ cat ~/.cache/wal/sequences &
 neofetch --disable gpu term_font cpu memory
 set fish_greeting
 set -x SSH_AUTH_SOCK '/run/user/1000/gnupg/S.gpg-agent.ssh'
+set PATH $HOME/.cargo/bin $PATH
